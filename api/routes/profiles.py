6da4cc246f3b0dc9c06461a8220d68d419e7c887
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-28 20:34:20 -0400

    Added method to update profiles

--- api/routes/profiles.py
+++ api/routes/profiles.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, Header, Query, Path, Resp
 from db import DatabaseManager, database_manager
 import dependencies
 import queries
-from models import CreateUser
+from models import CreateUser, UpdateProfile
 from shared import slap
 import mapping
 
@@ -33,7 +33,7 @@ async def get_profile_from_username(response: Response, username: Annotated[str,
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-@router.get("/profiles/username/{uuid}", tags=["Profiles"],
+@router.get("/profiles/uuid/{uuid}", tags=["Profiles"],
             name="Retrieve a profile by providing the unique user identifier",
             description="Providing the uuid as a parameter will allow users to retrieve the corresponding profile information. A signature and public key must be included in the header to verify the request date.")
 async def get_profile_from_uuid(response: Response, uuid: Annotated[str, Path()], 
@@ -59,6 +59,7 @@ async def register_profile(new_user: CreateUser, response: Response,
             db["users"].insert_one(user_db)
             profile_db, profile = mapping.create_generic_profile(user)
             db["profiles"].insert_one(profile_db)
+            response.status_code = status.HTTP_201_CREATED
             return vars(profile)
     except ValueError:
         raise HTTPException(status_code=401, detail="Public key and/or signature are not in correct format.")
@@ -67,9 +68,24 @@ async def register_profile(new_user: CreateUser, response: Response,
 
 @router.put("/profile/update/{uuid}", tags=["Profiles"],
             name="Update a profile")
-async def update_profile():
+async def update_profile(updated_profile: UpdateProfile, uuid: Annotated[str, Path()], response: Response,
+                        verification = Depends(dependencies.post_verify_signature),
+                        db : DatabaseManager = Depends(database_manager.get_database)):
     try:
-        pass
+        if not slap.verifySignature(verification["signature"], update_profile, verification["publicKey"]):
+            raise HTTPException(403, detail="Signature could not be verified.")
+        profile = queries.get_profile_from_uuid(db["profiles"].find(), uuid)
+        if profile:
+            filter_item = {"_id": profile["_id"]}
+            updated_data = {
+                "$set": {
+                    "username": updated_profile["username"],
+                    "description": updated_profile["description"]
+                }
+            }
+            db["profiles"].update_one(filter_item, updated_data)
+            response.status_code = status.HTTP_202_ACCEPTED
+            return {"message": "Item successfully updated"}
     except ValueError:
         raise HTTPException(status_code=401, detail="Public key and/or signature are not in correct format.")
     except Exception as e:
