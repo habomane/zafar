113d7839a4ae70d1189c2a4a54cc10d7a9e85327
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-30 19:30:43 -0400

    Switched to expo-router

---
+++ mobile/app/home.js
@@ -0,0 +1,10 @@
+
+import { useLocalSearchParams } from 'expo-router';
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function PageTwo() {
+  const { slug } = useLocalSearchParams();
+
+  return (<View><Text>SeCOND PAGE</Text><Link href="/">Go back </Link></View>);
+}
---
+++ mobile/app/index.js
@@ -0,0 +1,16 @@
+
+import { useLocalSearchParams } from 'expo-router';
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function Page() {
+  const { slug } = useLocalSearchParams();
+
+  return (
+    <View>
+  <Text>Blog post: {slug}</Text>
+  <Link href="/home">Home</Link>
+  </View>
+  )
+
+}
