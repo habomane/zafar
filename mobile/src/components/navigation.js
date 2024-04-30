4b56dc0ba6776d201ff71bba4d14c6daf2850f59
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-30 18:11:31 -0400

    Added files again

---
+++ mobile/src/components/navigation.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export default function Navigation()
+{
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
