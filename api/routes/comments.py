2983e2ac784e4c38e78df735822d7a097a3e3eac
omaneh <122321976+omaneh@users.noreply.github.com>
2024-05-02 20:41:02 -0400

    Added endpoints for comments

--- api/routes/comments.py
+++ api/routes/comments.py
@@ -13,4 +13,22 @@ router = APIRouter()
 async def get_comments(response: Response):
     return {"comments"}
 
-@router.get("/comments/{postKey}")
+@router.get("/comments/{postKey}", tags=["Comments"])
+async def get_comments_from_postKey():
+    return {""}
+
+@router.get("/comments/{commentKey}", tags=["Comments"])
+async def get_comments_from_commentKey():
+    return {""}
+
+@router.post("/comment", tags=["Comments"])
+async def post_comment():
+    return {""}
+
+@router.put("/comment/{commentKey}", tags=["Comments"])
+async def update_comment():
+    return {""}
+
+@router.delete("/comment/{commentKey}", tags=["Comments"])
+async def delete_comment():
+    return {""}
