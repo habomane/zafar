7175419d57cbbe848dac863be5482d8cd5b3d45f
omaneh <122321976+omaneh@users.noreply.github.com>
2024-05-02 20:31:54 -0400

    Added additional routes for posts

--- api/routes/posts.py
+++ api/routes/posts.py
@@ -12,3 +12,31 @@ router = APIRouter()
 @router.get("/posts", tags=["Posts"])
 async def get_posts(response: Response):
     return {"posts"}
+
+@router.get("/posts/{topicKey}", tags=["Posts"])
+async def get_posts_for_topic(topicKey):
+    return {""}
+
+@router.get("/posts/{username}", tags=["Posts"],
+            name="Get Posts For User Through Username")
+async def get_posts_username(username):
+    return {""}
+
+@router.get("/posts/{uuid}", tags=["Posts"],
+            name="Get Posts For User Through UUID")
+async def get_posts_uuid(uuid):
+    return {""}
+
+@router.post("/post", tags=["Posts"])
+async def create_post(new_post):
+    return {""}
+
+@router.put("/post/{postKey}", tags=["Posts"])
+async def update_post():
+    return {""}
+
+@router.delete("/post/{postKey}", tags=["Posts"])
+async def delete_post():
+    return {""}
+
+        
